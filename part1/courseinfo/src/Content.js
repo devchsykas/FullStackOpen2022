603fb46320da397dd03754cd73ac85bb92c42083
Christos <99770160+chsykas@users.noreply.github.com>
2022-12-12 22:55:57 +0200

    exercise 1.4 submission

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,11 +1,15 @@
-import Part from "./Part";
-
 const Content = (props) => {
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
     </div>
   );
 };
