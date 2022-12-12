735e9567cc1a13b6a145ad5056e68e2bc5ca3e50
Christos <99770160+chsykas@users.noreply.github.com>
2022-12-13 01:12:59 +0200

    exercise 1.5 submission

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,11 +1,12 @@
 const Total = (props) => {
+  // console.log(props);
   return (
     <div>
       <p>
         Number of exercises {""}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
+        {props.parts[0]["exercises"] +
+          props.parts[1]["exercises"] +
+          props.parts[2]["exercises"]}
       </p>
     </div>
   );
