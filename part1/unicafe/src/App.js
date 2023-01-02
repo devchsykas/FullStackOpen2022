92dccfeb9254e2ff086895c82e32c61840389acc
Christos <99770160+chsykas@users.noreply.github.com>
2023-01-02 12:31:00 +0200

    exercise 1.7 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,20 +17,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGoodClick = () => {
     // console.log("clicked the good button");
     setGood(good + 1);
+    setAll(all + 1);
   };
 
   const handleNeutralClick = () => {
     // console.log("clicked the neutral button");
     setNeutral(neutral + 1);
+    setAll(all + 1);
   };
 
   const handleBadClick = () => {
     // console.log("clicked the bad button");
     setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  const averageClicks = () => {
+    const avg = all ? (good - bad) / all : all;
+    return avg;
+  };
+
+  const positiveClicks = () => {
+    const positive = good ? (good * 100) / all : good;
+    return positive;
   };
 
   return (
@@ -43,6 +57,9 @@ const App = () => {
       <Statistic text="good" value={good} />
       <Statistic text="neutral" value={neutral} />
       <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={averageClicks()} />
+      <Statistic text="percentage" value={positiveClicks()} />
     </div>
   );
 };
